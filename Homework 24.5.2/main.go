cce2c5a7499cfb279ebfbc8e70f1a39be737ba1d
Oleg Makarov <macosib@yandex.ru>
2022-08-11 12:17:37 +0700

    fix 24

--- Homework 24.5.2/main.go
+++ Homework 24.5.2/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"math/rand"
 	"time"
 )
 
-func sortBabelArray(array ...int) []int {
-	fmt.Println("Исходный массив:", array)
+// sortBabelArray - Функция принимает любое количество аргументов типа int (array)
+// Сортирует значения (пузырьковый метод) в массиве int по убыванию.
+func sortBabelArray(array ...int) {
+	log.Println("Исходный массив:", array)
 
 OuterLoop:
 	for i := 0; i < len(array)-1; i++ {
@@ -22,11 +24,12 @@ OuterLoop:
 			break OuterLoop
 		}
 	}
-	fmt.Println("Отсортированный массив:", array)
-	return array
-
+	log.Println("Отсортированный массив:", array)
 }
 
+// getTestArray - Функция принимает аргумент типа int
+// и вовзращает массив случайных значений типа int в диапазоне от 0 до 100.
+// Длина массива равна значения получаемого аргумента.
 func getTestArray(size int) []int {
 	result := make([]int, size)
 	for i := 0; i < len(result); i++ {
@@ -35,13 +38,17 @@ func getTestArray(size int) []int {
 	return result
 }
 
+// getRandomNumber - Функция не принимает никаких аргументов
+// и вовзращает случайное число типа int в диапазоне от 0 до 100.
 func getRandomNumber() int {
-	return rand.Intn(10)
+	return rand.Intn(100)
 }
 
+// main - Основная функция программы, не принимает никаких аргументов.
+// Устанавливает начальную точку отсчета для модуля rand и выводит в консоль результат выполнения анонимной функции,
+// которая принимает любое количество аргументов типа int
+// и выводит в консоль результат отсортированный массив в обратном порядке.
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	massSort := func(args ...int) []int { return sortBabelArray(args...) }
-	massSort(getTestArray(getRandomNumber())...)
-	massSort()
+	func(args ...int) { sortBabelArray(args...) }(getTestArray(getRandomNumber())...)
 }
